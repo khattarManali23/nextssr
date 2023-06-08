dac06ccd36b50712dc9034d86ab857fa70348df3
khattarmanali <manalikhattar1@gmail.com>
2023-06-08 14:47:08 +0530

    first commit

---
+++ app/[page]/page.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+export default async function Page({ params }) {
+  console.log(params, "params");
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.page}`,
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await res.json();
+  return (
+    <>
+      <Head></Head>
+      <div className="grid grid-cols-6 gap-x-6 gap-y-3">
+        <div className="col-span-full space-y-3 lg:col-span-4">
+          <h1 className="truncate text-2xl font-medium capitalize text-gray-200">
+            {data.title}
+          </h1>
+          <p className="font-medium text-gray-500">{data.body}</p>
+        </div>
+      </div>
+    </>
+  );
+}
