df35398dcbd889a871414355679fb0dd71b01d0e
khattarmanali <manalikhattar1@gmail.com>
2023-06-09 10:39:13 +0530

    page props

--- app/[page]/page.js
+++ app/[page]/page.js
@@ -2,11 +2,11 @@ import Head from "next/head";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-export const metadata = {
-  title: "My page title",
-  description: "This will be the page meta description",
-  // ...
-};
+// export const metadata = {
+//   title: "My page mnali",
+//   description: "This will be the page meta description",
+//   // ...
+// };
 
 export default async function Page({ params }) {
   const res = await fetch(
@@ -45,4 +45,22 @@ export default async function Page({ params }) {
       </div>
     </>
   );
+}
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const id = params.id;
+
+  // fetch data
+  const product = await fetch(
+    `https://api-vande-rastra.vercel.app/api/v1/newsmanagement/allByCategory/${params.page}`
+  ).then((res) => res.json());
+  const data = product.data;
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: data[0].title,
+  };
 }
