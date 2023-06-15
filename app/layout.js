b5095dc8a72d00236e1f6d191ecab12aaee336e7
khattarmanali <manalikhattar1@gmail.com>
2023-06-15 10:41:35 +0530

    added google tag

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 "use client";
 
 import "./globals.css";
@@ -11,6 +12,7 @@ import useDeviceType from "@/custom-hooks/useDeviceType";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import store from "@/redux/store";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +33,42 @@ export default function RootLayout({ children }) {
   }
   return (
     <html lang="en">
+      <head>
+        <script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-Y0MJBV6WCF`}
+        />
+
+        <script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+
+    gtag('config', 'G-Y0MJBV6WCF', {
+      page_path: window.location.pathname,
+    });
+    `,
+          }}
+        />
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-ND5K6VT"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+
+              visibility: "hidden",
+            }}
+          ></iframe>
+        </noscript>
+      </head>
+
       <body>
         <QueryClientProvider client={queryClient}>
           <Provider store={store}>
